8b9296ed6825b247b32d181f5ec0fbed6153aa94
Saikat-D023 <saikatdey358@gmail.com>
2024-12-09 20:23:34 +0530

    selectors were coded

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom, evenSelector } from './store/atoms/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <RecoilRoot>
+        <Buttons />
+        <Counter />
+        <IsEven  />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Buttons(){
+
+  const setCount = useSetRecoilState(counterAtom)
+
+  function increase(){
+    setCount( c => c + 2)
+  }
+
+  function decrease(){
+    setCount( c => c - 1)
+  }
+
+   return <div>
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+   </div>
+}
+
+function Counter(){
+  //subscribing to the actual value
+  const count = useRecoilValue(counterAtom)
+
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven(){
+  //subscribing to the derived state , the selector
+  const even = useRecoilValue(evenSelector)
+
+  return <div>
+    {even ? "Even" : "Odd"}
+  </div>
 }
 
 export default App
